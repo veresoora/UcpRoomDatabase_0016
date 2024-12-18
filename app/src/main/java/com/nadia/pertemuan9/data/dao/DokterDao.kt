acf1307af7a5323902aa8b4ba46792a0c8cf07db
veresoora <lahanabira@gmail.com>
2024-12-18 17:35:56 +0700

    Membuat Dao untuk operasi pada tabel dokter

--- app/src/main/java/com/nadia/pertemuan9/data/dao/DokterDao.kt
+++ app/src/main/java/com/nadia/pertemuan9/data/dao/DokterDao.kt
@@ -1,4 +1,16 @@
 package com.nadia.pertemuan9.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.nadia.pertemuan9.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface DokterDao {
+    @Insert
+    suspend fun insertDokter(dokter: Dokter)
+
+    @Query("SELECT * FROM dokter ORDER BY nama ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
 }
