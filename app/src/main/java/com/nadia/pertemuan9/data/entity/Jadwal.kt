4d9bcefebbc0763a201cfc111bf37911924bf848
veresoora <lahanabira@gmail.com>
2024-12-18 17:21:05 +0700

    Membuat entity Jadwal

--- app/src/main/java/com/nadia/pertemuan9/data/entity/Jadwal.kt
+++ app/src/main/java/com/nadia/pertemuan9/data/entity/Jadwal.kt
@@ -1,3 +1,24 @@
 package com.nadia.pertemuan9.data.entity
 
-data class Jadwal()
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "jadwal",
+    foreignKeys = [
+        ForeignKey(
+            entity = Dokter::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("id"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
+data class Jadwal(
+    @PrimaryKey
+    val idjadwal: String,
+    val id: String,
+    val namapasien: String,
+    val nomorhp: String,
+    val tglkonsul: String,
+    val status: String,
+)
