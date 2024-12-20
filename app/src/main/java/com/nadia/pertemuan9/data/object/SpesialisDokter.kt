9e149a84d1b5f2e401895b1ff1f0831a2c9884da
veresoora <lahanabira@gmail.com>
2024-12-20 19:31:48 +0700

    Menambahkan object "SpesialisDokter" untuk daftar spesialis dokter

---
+++ app/src/main/java/com/nadia/pertemuan9/data/object/SpesialisDokter.kt
@@ -0,0 +1,10 @@
+package com.nadia.pertemuan9.data.object
+
+object SpesialisDokter {
+    val options = listOf(
+        "Dokter Spesialis Umum",
+        "Dokter Spesialis Anak",
+        "Dokter Spesialis Bedah",
+        "Dokter Spesialis Obgyn"
+    )
+}
