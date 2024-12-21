0a883b7f88ccad4e4a8a84d601d8a1fb7c65fa1c
veresoora <lahanabira@gmail.com>
2024-12-22 03:37:52 +0700

    Menambahkan fungsi getNamaDokter ke RepositoryJadwal

--- app/src/main/java/com/nadia/pertemuan9/repository/RepositoryJadwal.kt
+++ app/src/main/java/com/nadia/pertemuan9/repository/RepositoryJadwal.kt
@@ -1,5 +1,6 @@
 package com.nadia.pertemuan9.repository
 
+import com.nadia.pertemuan9.data.entity.Dokter
 import com.nadia.pertemuan9.data.entity.Jadwal
 import kotlinx.coroutines.flow.Flow
 
@@ -9,6 +10,8 @@ interface RepositoryJadwal {
     fun getAllJadwal () : Flow<List<Jadwal>>
 
     fun getJadwal(idjadwal: Int) : Flow<Jadwal>
+
+    fun getNamaDokter() : Flow<List<Dokter>>
 
     suspend fun updateJadwal(jadwal: Jadwal)
 
