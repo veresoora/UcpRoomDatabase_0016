29cb007b7a2794ba23c9a1c553de8f46a6209ee1
veresoora <lahanabira@gmail.com>
2024-12-22 04:38:02 +0700

    Memanggil fungsi PengelolaHalaman ke MainActivity

--- app/src/main/java/com/nadia/pertemuan9/MainActivity.kt
+++ app/src/main/java/com/nadia/pertemuan9/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.nadia.pertemuan9.ui.navigation.PengelolaHalaman
 import com.nadia.pertemuan9.ui.theme.Pertemuan9Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan9Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
