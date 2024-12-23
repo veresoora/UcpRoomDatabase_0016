8e3fbd7cc74fce4dee8e3ca00ba448d2238a4bee
veresoora <lahanabira@gmail.com>
2024-12-23 13:31:38 +0700

    Menambahkan 3 route yaitu AlamatHomeJadwal, AlamatDetailJadwal, dan AlamatEditJadwal

--- app/src/main/java/com/nadia/pertemuan9/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/nadia/pertemuan9/ui/navigation/PengelolaHalaman.kt
@@ -10,6 +10,10 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.nadia.pertemuan9.ui.view.dokter.HomeDokterView
 import com.nadia.pertemuan9.ui.view.dokter.InsertDokterView
+import com.nadia.pertemuan9.ui.view.jadwal.DetailJadwalView
+import com.nadia.pertemuan9.ui.view.jadwal.HomejadwalView
+import com.nadia.pertemuan9.ui.view.jadwal.InsertJadwalView
+import com.nadia.pertemuan9.ui.view.jadwal.UpdateJadwalView
 
 @Composable
 fun PengelolaHalaman(
@@ -46,6 +50,83 @@ fun PengelolaHalaman(
                     navController.popBackStack()
                 },
                 modifier = modifier
+            )
+        }
+
+        composable(
+            route = AlamatHomeJadwal.route
+        ) {
+            HomejadwalView(
+                onDetailClick = { idjadwal ->
+                    navController.navigate("${AlamatDetailJadwal.route}/$idjadwal")
+                    println("PengelolaHalaman: idjadwal = $idjadwal")
+                },
+                onAddJadwal = {
+                    navController.navigate(AlamatTambahJadwal.route)
+                },
+                navigateLihatDokter = {
+                    navController.navigate(AlamatHomeDokter.route)
+                },
+                modifier = modifier
+            )
+        }
+
+        composable(
+            route = AlamatTambahJadwal.route
+        ) {
+            InsertJadwalView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier
+            )
+        }
+
+        composable (
+            AlamatDetailJadwal.routeWithArg,
+            arguments = listOf(
+                navArgument(AlamatDetailJadwal.IDJWL) {
+                    type = NavType.IntType
+                }
+            )
+        ){
+            val idjadwal = it.arguments?.getInt(AlamatDetailJadwal.IDJWL)
+
+            idjadwal?.let { idjadwal ->
+                DetailJadwalView(
+                    onBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${AlamatEditJadwal.route}/$idjadwal")
+                    },
+                    modifier = modifier,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+
+        composable(
+            AlamatEditJadwal.routeWithArg,
+            arguments = listOf(
+                navArgument (AlamatEditJadwal.IDJWL) {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            UpdateJadwalView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
             )
         }
     }
